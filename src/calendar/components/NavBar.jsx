3616330ef347cebb0d5911bf46e5cd07eaeb70ee
NoeGJ <noargoji@gmail.com>
2025-03-09 15:09:19 -0600

    Fix

--- src/calendar/components/NavBar.jsx
+++ src/calendar/components/NavBar.jsx
@@ -10,7 +10,7 @@ export const NavBar = () => {
         <span className="navbar-brand">
             <i className="fas fa-calendar-alt" ></i>
             &nbsp;
-            { user.name }
+            { user.username }
         </span>
 
         <button 
--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -22,6 +22,9 @@ export const useCalendarStore = () => {
 
         try {
             const { activities, ...event } = calendarEvent
+
+            
+            
             
             if( calendarEvent.id ){
                 //updating
